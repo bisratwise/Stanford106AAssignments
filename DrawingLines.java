fb774e311028a6e84b59de435d64aa28ecd41b33
bisratwise <79027405+bisratwise@users.noreply.github.com>
2021-02-22 14:18:40 -0800

    Add files via upload

---
+++ DrawingLines.java
@@ -0,0 +1,35 @@
+package week2;
+
+import java.awt.event.MouseEvent;
+
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
+
+import acm.program.GraphicsProgram;
+
+public class DrawingLines extends GraphicsProgram {
+
+	public void run() {
+		addMouseListeners();
+	}
+
+	/** Called on mouse press to create a new line */
+	public void mousePressed(MouseEvent e) {
+		double x = e.getX();
+		double y = e.getY();
+		line = new GLine(x, y, x, y);
+		add(line);
+	}
+
+	/** Called on mouse drag to reset the endpoint */
+	public void mouseDragged(MouseEvent e) {
+		double x = e.getX();
+		double y = e.getY();
+		line.setEndPoint(x, y);
+	}
+
+	/* Private instance variables */
+	private GLine line;
+
+}
+
